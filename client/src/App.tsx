7f4bae196e85b91ad5d4c6ff0081e69d6f5e6d46
Karen <lakarina86@gmail.com>
2022-03-11 18:24:43 +0100

    refactor: pethotels component to ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home/home";
 import Nav from "./components/nav/nav";
 import DogSitter from "./components/dogSitter/dogSitter";
 import Login from "./components/login/login";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Map } from "./components/Map/Map";
-import PetHotels from "./components/petHotels/petHotels";
+import { PetHotels } from "./components/PetHotels/PetHotels";
 
 const App: React.FC = () => {
   return (
