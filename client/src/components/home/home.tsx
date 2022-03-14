9d86680f8321b26e5770771754ba8b6e2c64bddb
Karen <lakarina86@gmail.com>
2022-03-14 19:07:18 +0100

    chore: remove unused componenets and improve gral style

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import CTA from "../CTA/CTA";
 import puppiesSuitcase from "../../static/images/transparent.png";
-import HomeSocials from "../HomeSocials/HomeSocials";
 import { HomeDiscoverButtons } from "../HomeDiscoverButtons/HomeDiscoverButtons";
 import "./Home.css";
 
@@ -12,9 +9,7 @@ const Home: React.FC = () => {
       <div className="container header__container">
         <h1> DOGGO </h1>
         <h3> Make it their holiday too </h3>
-        {/* <CTA /> */}
         <HomeDiscoverButtons />
-        {/* <HomeSocials /> */}
         <div className="landingPhoto">
           <img
             src={puppiesSuitcase}
@@ -22,15 +17,6 @@ const Home: React.FC = () => {
             className="dogPic"
           />
         </div>
-
-        {/* <a href="#dogSitter" className="scroll__down">
-          Scroll Down
-        </a> */}
-
-        {/* <Link to="sitter" >
-          <div className="scroll__down">Scroll Down</div>
-        </Link> */}
-
       </div>
     </header>
   );
