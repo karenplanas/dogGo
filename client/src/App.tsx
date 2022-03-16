d13b6c375aa5956de5c1eee69e87d2370001795f
Karen <lakarina86@gmail.com>
2022-03-16 11:52:00 +0100

    feat: refactor petHotels using react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Nav } from "./components/Nav/Nav";
 import { Sitter } from "./components/Sitter/Sitter";
@@ -9,22 +10,26 @@ import { Home } from "./components/Home/Home";
 import { UserContextProvider } from "./context/UserContext";
 import './App.css'
 
+const queryClient = new QueryClient()
+
 const App: React.FC = () => {
   return (
-    <UserContextProvider>
-      <BrowserRouter>
-        <div className="App">
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="petHotels" element={<PetHotels />} />
-            <Route path="map" element={<Map />} />
-            <Route path="sitter" element={<Sitter />} />
-            <Route path="login" element={<Login />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </UserContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <BrowserRouter>
+          <div className="App">
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="petHotels" element={<PetHotels />} />
+              <Route path="map" element={<Map />} />
+              <Route path="sitter" element={<Sitter />} />
+              <Route path="login" element={<Login />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </UserContextProvider>
+    </QueryClientProvider>
   );
 };
 
